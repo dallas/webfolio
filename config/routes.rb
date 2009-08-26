d6aa976bba1be699367adc76b41dd908ded1cb8e
Dallas Reedy <code@dallasreedy.com>
2009-08-25 20:58:53 -0700

    downgrading to Rails 2.3.2 (let’s see if this works…)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.with_options :controller => :application do |app|
-    app.contact   'contact',    :action => :contact
-    app.portfolio ':portfolio', :action => :index,    :requirements => { :portfolio => /web|print|art/ }
-    app.root                    :action => :redirect
+  map.with_options :controller => 'application' do |app|
+    app.contact   'contact',    :action => 'contact'
+    app.portfolio ':portfolio', :action => 'index',    :requirements => { :portfolio => /web|print|art/ }
+    app.root                    :action => 'redirect'
   end
 end
