161c8a4cf15335d8d9889568fcf78ac34495b7c2
Dallas Reedy <code@dallasreedy.com>
2010-04-25 17:09:11 -0700

    removing the rails_app directory, I no longer need it immediately present

--- rails_app/config/routes.rb
+++
@@ -1,7 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.with_options :controller => :application do |app|
-    app.contact   'contact',    :action => :contact
-    app.portfolio ':portfolio', :action => :index,    :requirements => { :portfolio => /web|print|art/ }
-    app.root                    :action => :redirect
-  end
-end
