0b647b649014ead927d3eb07649e1ff5ba9b4a67
Dallas Reedy <code@dallasreedy.com>
2012-01-13 08:14:43 -0800

    working on stuff…

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source :rubygems
 
+gem 'rake'
 gem 'sinatra', :require => 'sinatra/base'
 gem 'haml'
 gem 'sass'
+gem 'bourbon'
--- svc/app.rb
+++ svc/app.rb
@@ -5,12 +5,20 @@ module SVC
     end
 
     get '/html+css/?:view?' do |view|
-      case view
-      when nil, 'index', 'home' then view = :index
-      when /week-?([1-5])/      then view = :"week#{$1}"
-      end
+      haml :"html-and-css/index", :layout => false
+    end
 
-      haml :"html-and-css/#{view}", :layout => :"html-and-css/layout"
+    # Stylesheets
+    get '/css/:stylesheet.css' do |stylesheet|
+      pass unless have_sass_file?(stylesheet)
+      content_type 'text/css', :charset => 'utf-8'
+      scss :"css/#{stylesheet}", :style => :nested
+    end
+
+    def have_sass_file?(stylesheet)
+      files = Dir[File.expand_path(File.join(File.dirname(__FILE__), 'views', 'css', '*.scss'))]
+      files = files.map { |file| File.basename(file, '.scss') }
+      files.include?(stylesheet)
     end
   end
 end
